9df0297b30601c9f5b7912cb22c7560e61b986fd
isheikh8492 <isheikh8492@gmail.com>
2022-09-22 11:36:46 -0500

    brought login form to center

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./component/Login/Login.jsx";
 import './App.css';
 
--- src/component/Login/Login.jsx
+++ src/component/Login/Login.jsx
@@ -13,6 +13,28 @@ export default class Login extends Component {
                 </div>
                 <div class="Right">
                     <Navbar />
+                    <div class="loginForm">
+                        <div className="card col-12 col-lg-4 login-card mt-2 hv-center">
+                            <form id="form-container">
+                                <h5>Sign In</h5>
+                                <p>
+                                    <input type="text" id="username" placeholder="username" />
+                                </p>
+                                <p>
+                                    <input type="password" id="password" placeholder="password" />
+                                </p>
+                                <p>
+                                    <button id="submitButton" type="button">Se connecter</button>
+                                </p>
+                            </form>
+                            <div className="alert alert-success mt-2" role="alert">
+                            </div>
+                            <div className="registerMessage">
+                                <span>Don't have an account? </span>
+                                <span className="loginText">Register</span>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         );
